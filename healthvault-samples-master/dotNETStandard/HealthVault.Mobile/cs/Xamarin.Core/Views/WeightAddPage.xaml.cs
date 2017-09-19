bb5cdbae862fbb65c2e19aa6e73bf27a497e4a3e
unknown <DevenHurt@gmail.com>
2017-09-19 13:42:59 -0400

    Added sample projects. Need to update product id

---
+++ healthvault-samples-master/dotNETStandard/HealthVault.Mobile/cs/Xamarin.Core/Views/WeightAddPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HealthVault.Sample.Xamarin.Core.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WeightAddPage : ContentPage
+    {
+        public WeightAddPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ healthvault-samples-master/dotNETStandard/HealthVault.Mobile/cs/Xamarin.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthVault.Sample.Xamarin.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            LoadApplication(new Core.App());
+        }
+    }
+}
