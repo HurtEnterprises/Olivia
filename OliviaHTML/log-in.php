98c6c1bc1948b8a1b4a733916ed10dcc66c96892
DevenHurt <DevenHurt@gmail.com>
2018-01-19 17:43:52 -0500

    Azure Login Done + Doctor Side Mocks Added

---
+++ OliviaHTML/log-in.php
@@ -0,0 +1,74 @@
+﻿<!DOCTYPE html>
+<html class="nojs html css_verticalspacer" lang="en-US">
+ <head>
+
+  <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
+  <meta name="generator" content="2018.0.0.379"/>
+  
+  <script type="text/javascript">
+   // Update the 'nojs'/'js' class on the html node
+document.documentElement.className = document.documentElement.className.replace(/\bnojs\b/g, 'js');
+
+// Check that all required assets are uploaded and up-to-date
+if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required":["museutils.js", "museconfig.js", "require.js", "webpro.js", "jquery.watch.js", "log-in.css"], "outOfDate":[]};
+</script>
+  
+  <title>Log In</title>
+  <!-- CSS -->
+  <link rel="stylesheet" type="text/css" href="css/site_global.css?crc=223211964"/>
+  <link rel="stylesheet" type="text/css" href="css/log-in.css?crc=478198703" id="pagesheet"/>
+   </head>
+ <body>
+  <div class="clearfix" id="page"><!-- group -->
+   <div class="clearfix grpelem" id="pu730"><!-- group -->
+    <div class="clip_frame" id="u730"><!-- image -->
+     <img class="block" id="u730_img" src="images/green9-crop-u730.jpg?crc=54388743" alt="" width="1529" height="659"/>
+    </div>
+    <a class="nonblock nontext clip_frame" id="u723" href="help.html"><!-- image --><img class="block" id="u723_img" src="images/olivia%20white110x94.png?crc=395428233" alt="" width="110" height="93"/></a>
+   </div>
+   <div class="clearfix grpelem" id="pu726-4"><!-- column -->
+    <div class="Paragraph-Style clearfix colelem" id="u726-4"><!-- content -->
+     <p>WELCOME</p>
+    </div>
+    <div class="colelem" id="u729"><!-- simple frame --></div>
+    <div class="clearfix colelem" id="u727-4"><!-- content -->
+     <p>Please log in below:</p>
+    </div>
+    <div class="clearfix colelem" id="pwidgetu4210"><!-- group -->
+     <form class="form-grp clearfix grpelem" id="widgetu4210" method="POST" enctype="multipart/form-data" action="scripts/loginScript.php"><!-- none box -->
+      <div class="clearfix grpelem" id="u4216-4"><!-- content -->
+       <p>Submitting Form...</p>
+      </div>
+      <div class="clearfix grpelem" id="u4211-4"><!-- content -->
+       <p>The server encountered an error.</p>
+      </div>
+      <div class="clearfix grpelem" id="u4229-4"><!-- content -->
+       <p>Form received.</p>
+      </div>
+      <button class="submit-btn NoWrap rounded-corners clearfix grpelem" id="u4230-4" type="submit" value="SUBMIT" tabindex="3"><!-- content -->
+       <div style="margin-top:-18px;height:18px;">
+        <p>SUBMIT</p>
+       </div>
+      </button>
+      <div class="fld-grp clearfix grpelem" id="widgetu4217" data-required="true"><!-- none box -->
+       <label class="fld-label actAsDiv clearfix grpelem" id="u4218-4" for="widgetu4217_input"><!-- content --><span class="actAsPara">Email:</span></label>
+       <span class="fld-input NoWrap actAsDiv clearfix grpelem" id="u4220-4"><!-- content --><input class="wrapped-input" type="text" id="widgetu4217_input" placeholder="Please Enter Your Email" name="email" tabindex="1"/></span>
+      </div>
+      <div class="fld-grp clearfix grpelem" id="widgetu4221" data-required="true"><!-- none box -->
+       <label class="fld-label actAsDiv clearfix grpelem" id="u4224-4" for="widgetu4221_input"><!-- content --><span class="actAsPara">Password:</span></label>
+       <span class="fld-input NoWrap actAsDiv clearfix grpelem" id="u4222-4"><!-- content --><input class="wrapped-input" type="password" id="widgetu4221_input" placeholder="Please Enter Your Password" name="password" tabindex="2"/></span>
+      </div>
+     </form>
+     <a class="nonblock nontext rounded-corners grpelem" id="u4536" href=""><!-- simple frame --></a>
+    </div>
+    <div class="clearfix colelem" id="u728-4"><!-- content -->
+     <p><span id="u728">please click here if you have trouble logging in</span></p>
+    </div>
+    <div class="clearfix colelem" id="u725-4"><!-- content -->
+     <p>by Hurt Technologies, Inc.</p>
+    </div>
+   </div>
+   <div class="verticalspacer" data-offset-top="536" data-content-above-spacer="585" data-content-below-spacer="30"></div>
+  </div>
+   </body>
+</html>
---
+++ OliviaHTML/scripts/loginScript.php
@@ -0,0 +1,67 @@
+<?php 
+	// add in error reporting
+	ini_set('display_errors','On');
+	ini_set('html_errors',0);
+	error_reporting(-1);
+	//require('form_process.php');
+	//require('../vendor/autoload.php');
+	//use WindowsAzure\Common\ServicesBuilder;
+
+	if ($_SERVER["REQUEST_METHOD"] == "GET") //Switch to get
+    {
+        // else render form
+		header("Location: ../log-in.php");die();
+
+    }  
+	elseif ($_SERVER["REQUEST_METHOD"] == "POST")  //switch to post
+	{
+		//header("Location: ../question-explanation.html");die();
+		if (empty($_POST["email"]) or empty($_POST["password"]))
+        {
+            header("Location: ../log-in.php");die();
+        }
+        else 		
+		{
+			$loginname = trim($_POST["email"]);
+			$loginpassword = trim($_POST["password"]);
+			//Verify login input
+			//Establishes the connection
+			$host = "olivialogs.mysql.database.azure.com";
+			$username = "devenhurt@olivialogs";
+			$password = "LongLiveMedKit!";
+			$db_name = "patient";
+			$conn = mysqli_init();
+			mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+			if (mysqli_connect_errno($conn)) {
+			die('Failed to connect to MySQL: '.mysqli_connect_error());
+			}
+
+			//Run the Select query
+			/*printf("Reading data from table: \n");
+			$res = mysqli_query($conn, 'SELECT * FROM logins');
+			while ($row = mysqli_fetch_assoc($res)) {
+			var_dump($row);
+			}*/
+			$loginname = mysqli_real_escape_string($conn,$loginname);
+			$loginpassword = mysqli_real_escape_string($conn,$loginpassword);
+			echo($loginname);
+			echo($loginpassword);
+			$result = mysqli_query($conn,"SELECT * FROM logins where name='$loginname' and password='$loginpassword'")or die(mysql_error());
+			if(!$result || mysqli_num_rows($result) <= 0)
+			{
+				echo("invalid user");
+			}
+			else
+			{
+				echo("successful login");
+				session_start();
+				$_SESSION["user"] = $loginname;
+				header("Location: ../question-explanation.html");
+			}
+
+			//Close the connection
+			mysqli_close($conn); 
+		}
+	}
+     
+?>
